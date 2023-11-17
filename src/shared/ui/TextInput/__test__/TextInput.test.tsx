2844670dc3d47c9a5ee12aaa54aa634f9123983d
Dima <fox6383@gmail.com>
2023-11-17 09:19:14 +0200

    feat: add some TextInput test

--- src/shared/ui/TextInput/__test__/TextInput.test.tsx
+++ src/shared/ui/TextInput/__test__/TextInput.test.tsx
@@ -1,9 +1,36 @@
-import { expect, test } from "vitest";
+import { expect, describe, it } from "vitest";
 import { TextInput } from "..";
 import { render } from "@testing-library/react";
 
-test("render component", () => {
-  const { getByLabelText } = render(<TextInput label="Email" />);
+describe("TextInput", () => {
+  describe("root element", () => {
+    it("should be in the document", () => {
+      const { getByLabelText } = render(<TextInput label="Email" />);
+      expect(getByLabelText("Email")).toBeInTheDocument();
+    });
 
-  expect(getByLabelText("Email")).toBeInTheDocument();
+    it("should contain `type` attribute with `text value`", () => {
+      const { getByLabelText } = render(<TextInput label="Email" />);
+      expect(getByLabelText("Email")).toHaveAttribute("type", "text");
+    });
+
+    describe("when placeholder property", () => {
+      describe("is set", () => {
+        const placeholder = "placeholder";
+
+        describe("root element", () => {
+          it("should contain 'placeholder' attribute with 'placeholder' property as value", () => {
+            const { getByLabelText } = render(
+              <TextInput placeholder={placeholder} label="Email" />
+            );
+
+            expect(getByLabelText("Email")).toHaveAttribute(
+              "placeholder",
+              placeholder
+            );
+          });
+        });
+      });
+    });
+  });
 });
